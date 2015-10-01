524dc000e174868c8a811cafa47814efd6a044a4
Anita Kumari <akumari@argusoft.com>
2015-10-01 16:52:35 +0530

    made _all_ids local to self executing block and provided _all_ids access to only id() function.

--- public_html/id.js
+++ public_html/id.js
@@ -8,69 +8,73 @@
  *  It'll not throw error if we use typeof as function,but that's not correct.
  *   
  */
-if (typeof NAMESPACE === 'undefined' || NAMESPACE === null) {
-    console.log("namespace reloaded")
-    NAMESPACE = {};
-}
 
-if (typeof NAMESPACE.id === 'undefined' || NAMESPACE.id === null) {
+(function() {
+    
+    if (typeof NAMESPACE === 'undefined' || NAMESPACE === null) {
+        NAMESPACE = {};
+    }
 
-    /**
-     * id() function will be associated with the user and can be called for specific user id, but
-     * we want _all_ids to be global so that we can get the all active logged in user ids.
-     */
-    _all_ids = new Array();
-
-    var id = function(id) {
-
-        var persona = {};
-        var _id = id;
+    if (typeof NAMESPACE.id === 'undefined' || NAMESPACE.id === null) {
 
         /**
-         * Stores the user login id into active user array list.
+         * id() function will be associated with the user and can be called for specific user id, but
+         * As you have mentioned that _all_ids should be protected so that others can't access that, but still allow each id instance to see a single _all_ids,
+         * So we have made _all_ids to be local to self execuiting block and id function can have single _all_ids access.
          */
-        _all_ids.push(_id);
+      var _all_ids = new Array();
 
-        var getId = function() {
-            return _id;
+        var id = function(id) {
+
+            var persona = {};
+            var _id = id;
+
+            /**
+             * Stores the user login id into active user array list.
+             */
+            _all_ids.push(_id);
+
+            var getId = function() {
+                return _id;
+            };
+
+            persona.getId = getId;
+            var _closed = false;
+
+            var close = function() {
+
+                /**
+                 * delete operator on array, only deletes the array element, but does not modifies array length. 
+                 * splice(using element index) deletes the element and also accordingly updates array length.
+                 */
+                var index = _all_ids.indexOf(getId());
+                _all_ids.splice(index, 1);
+
+
+                /**
+                 *  Here this keyword refer to global window object, which is different from the id() function scope. 
+                 *  So the _closed variable used at line 66 in this file, and the _closed variable used at line 32 in this file, 
+                 *  will be different variables at different scope levels.
+                 */
+                _closed = true;
+
+                /**
+                 * persona.close store the value what is returned by the close() method, but close() method doesn't return any thing.
+                 * And so the property close of persona object doesn't stored anything. 
+                 * The close() method should return value,that will be stored in persona.close variable. 
+                 * And as here _closed variable is unused , so that can be the value, which should be retuned by this method.
+                 */
+                return _closed;
+            };
+
+            persona.close = close;
+
+            return persona;
         };
 
-        persona.getId = getId;
-        var _closed = false;
-
-        var close = function() {
-
-            /**
-             * delete operator on array, only deletes the array element, but does not modifies array length. 
-             * splice(using element index) deletes the element and also accordingly updates array length.
-             */
-            var index = _all_ids.indexOf(getId());
-            _all_ids.splice(index, 1);
-
-
-            /**
-             *  Here this keyword refer to global window object, which is different from the id() function scope. 
-             *  So the _closed variable used at line 66 in this file, and the _closed variable used at line 32 in this file, 
-             *  will be different variables at different scope levels.
-             */
-            _closed = true;
-
-            /**
-             * persona.close store the value what is returned by the close() method, but close() method doesn't return any thing.
-             * And so the property close of persona object doesn't stored anything. 
-             * The close() method should return value,that will be stored in persona.close variable. 
-             * And as here _closed variable is unused , so that can be the value, which should be retuned by this method.
-             */
-            return _closed;
-        };
-
-        persona.close = close;
-
-        return persona;
-    };
-
-    NAMESPACE['id'] = id;
-}
+        NAMESPACE['id'] = id;
+    }
+})();
 
 
 //-------------------------------------------TEST CASES-------------------------------------
@@ -94,31 +98,18 @@ QUnit.test('User Login', function(assert) {
     assert.strictEqual(user1_ref.getId(), 2, 'Got logged in user id');
 
     /**
-     * We can get the logged in user count any time using the user_ref variable
-     */
-    assert.strictEqual(_all_ids.length, 1, 'Only 1 user is logged in');
-
-    /**
      * Another user logs in the application
      */
     var user2_ref = NAMESPACE['id'](3);
 
     assert.strictEqual(user2_ref.getId(), 3, 'Another User Loged in with id 3');
 
-    /**
-     * logged in user count is checked, now it should be 2
-     */
-    assert.strictEqual(_all_ids.length, 2, 'Logged in user count is 2');
 
     /**
      * First user logs out from the system
      */
     assert.strictEqual(user1_ref.close(), true, 'User 1 logs out of the system');
 
-    /**
-     * logged in user count is checked, now it should be 1
-     */
-    assert.strictEqual(_all_ids.length, 1, 'Logged in user count is 1');
 });
 
 
